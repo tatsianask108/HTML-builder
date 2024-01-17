ee2346abfb3b150372706010cf0b66c9c4a19c78
tatsianask108 <tt.laurie@gmail.com>
2024-01-17 18:11:33 +0300

    feat: complete 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const folderPath = './03-files-in-folder/secret-folder';
+
+async function outputToConsole() {
+  try {
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
+
+    files.forEach(async (file) => {
+      if (file.isFile()) {
+        const filePath = path.join(folderPath, file.name);
+        const { size } = await fs.stat(filePath);
+        const fileExtension = path.extname(file.name).slice(1);
+
+        console.log(`${file.name} - ${fileExtension} - ${size}B`);
+      }
+    });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+outputToConsole();
